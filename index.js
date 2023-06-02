a0af559ef224685b59a04fdc23927975b6df861b
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2023-06-02 20:41:46 +0300

    trying different command combinations

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ bot.command('howtopay', ctx => {
   })
 })
 
-bot.command('how to pay', ctx => {
+bot.command('how_to_pay', ctx => {
   bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to ExamTime.\nI here are the instructions for paying on EXAMTIME', {
   })
 })
